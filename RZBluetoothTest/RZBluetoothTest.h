e9ecbf62f70001ce11f1f50e9ecc7e7b6abd037d
Josh Brown <josh.brown@e-gineering.com>
2018-09-06 15:44:02 -0400

    create RZBluetoothTest static library

---
+++ RZBluetoothTest/RZBluetoothTest.h
@@ -0,0 +1,13 @@
+//
+//  RZBluetoothTest.h
+//  RZBluetoothTest
+//
+//  Created by Josh Brown on 9/6/18.
+//  Copyright © 2018 Raizlabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RZBluetoothTest : NSObject
+
+@end
---
+++ RZBluetoothTest/RZBluetoothTest.m
@@ -0,0 +1,13 @@
+//
+//  RZBluetoothTest.m
+//  RZBluetoothTest
+//
+//  Created by Josh Brown on 9/6/18.
+//  Copyright © 2018 Raizlabs. All rights reserved.
+//
+
+#import "RZBluetoothTest.h"
+
+@implementation RZBluetoothTest
+
+@end
